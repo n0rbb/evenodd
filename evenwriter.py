1282d711d126130f823510f76385f0e65b32bd1c
Mario de Miguel <57144439+n0rbb@users.noreply.github.com>
2022-12-17 12:18:31 +0100

    Add files via upload

---
+++ evenwriter.py
@@ -0,0 +1,16 @@
+f = open("even.py", "a+")
+
+f.write("n = int(input(\"Please enter n: \")) \n")
+f.write("if n == 1: \n")
+f.write("\t" + "print(\"odd\")".strip() + "\n")
+
+n = int(input("Max int: "))
+
+for i in range(n - 1):
+    f.write("elif n == {}: \n".format(i + 2))
+    if (i + 2) % 2 == 0:
+        f.write("\t" + "print(\"even\")".strip() + "\n")
+    else:
+        f.write("\t" + "print(\"odd\")".strip() + "\n")
+
+f.close()
